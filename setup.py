4138c1c8041d3e46a0447075adb029f2ea98ac6b
BrendaLPhys <brenda.leyva89@gmail.com>
2020-10-22 17:12:33 -0700

    updating

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
   author_email = 'brenda.leyva89@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/BrendaLPhys/ProgrammingForDS',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['montecarlo', 'functions', 'lib'],   # Keywords that define your package best
+  keywords = ['numbers', 'sum', 'text'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          're',
+          're'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
