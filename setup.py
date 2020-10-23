5c5f174f69c171f191d70f5075d02ef6588d20c6
BrendaLPhys <brenda.leyva89@gmail.com>
2020-10-22 19:12:00 -0700

    changing

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ lic = licenses[cfg['license']]
 min_python = cfg['min_python']
 
 setuptools.setup(
-    name = cfg['lib_name'],
+    name = cfg['numbers_in_text'],
     license = lic[0],
     classifiers = [
         'Development Status :: ' + statuses[int(cfg['status'])],
