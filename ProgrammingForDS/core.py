ce73010e1e05b942b816906dc61e12132a1f56c2
BrendaLPhys <brenda.leyva89@gmail.com>
2020-10-22 17:51:11 -0700

    Trying final update again

--- ProgrammingForDS/core.py
+++ ProgrammingForDS/core.py
@@ -4,10 +4,8 @@ __all__ = ['number_sum']
 
 # Cell
 def number_sum(text):
-
     for line in text:
         y = re.findall("[0-9]+", text)
-
     sums = 0
     for list in y:
         number = int(list)
